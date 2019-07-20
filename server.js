14a05d8f9fb452e7123d0599fe4e6efddb0a6812
Steffano Malaga <smalagadev@gmail.com>
2019-07-20 12:53:04 -0400

    Comments added

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// Initialize Server 
+// Declare dependencies and variables
 const express = require('express');
-
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Initialize Server
 app.listen(PORT,  () => console.log(`Server is running on port ${PORT}`));
-
-
-
