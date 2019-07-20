b031e7a64036a4bb7f60371ad0b63fce5b99ba18
Tbattle1911 <mr.battle1911@gmail.com>
2019-07-20 12:31:19 -0400

    Initialize Server

---
+++ server.js
@@ -0,0 +1,11 @@
+// Initialize Server 
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,  () => console.log(`Server is running on port ${PORT}`));
+
+
+
