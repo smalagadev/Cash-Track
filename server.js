6af584688c8e1b5ae6904c6711411eb8c056bf83
Tbattle1911 <mr.battle1911@gmail.com>
2019-07-25 17:16:55 -0400

    Routes

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 // Initialize Server
 app.listen(PORT,  () => console.log(`Server is running on port ${PORT}`));
+
+//routes
+app.get('/', function(req,res){
+    res.send("This is the Home Page")
+});
+
+app.get('/log-in', function(req,res){
+    res.send(" This is the Log-In Page")
+});
+
+app.get('/sign-up', function(req,res){
+        res.send(" This is the Sign-up Page")
+});
