364647471d5ae27d28ccecda28682458bc9a5aca
Steffano Malaga <smalagadev@gmail.com>
2019-07-25 20:25:19 -0400

    07/25/19 Heroku push

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Declare dependencies and variables
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -9,7 +10,7 @@ app.listen(PORT,  () => console.log(`Server is running on port ${PORT}`));
 
 //routes
 app.get('/', function(req,res){
-    res.send("This is the Home Page")
+    res.sendFile(path.join(__dirname, '/cash-track/public/index.html');
 });
 
 app.get('/log-in', function(req,res){
