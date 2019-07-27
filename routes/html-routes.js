3fb0d15540866cf32f57a74c0bc9ccc3f206a77c
Tbattle1911 <mr.battle1911@gmail.com>
2019-07-27 12:09:14 -0400

    updated route folder

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+//path
+const path = require("path");
+
+//exports
+exports.modules = function(app){
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/cash-track/public/index.html"));
+  });
+
+  app.get("/log-in", function(req, res) {
+    res.send(" This is the Log-In Page");
+  });
+
+  app.get("/sign-up", function(req, res) {
+    res.send(" This is the Sign-up Page");
+  });
+};
+
+//routes
--- server.js
+++ server.js
@@ -5,18 +5,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
+require('./routes/api-routes.js')(app);
+
+require('./routes/html-routes.js')(app);
+
+
+
+
 // Initialize Server
-app.listen(PORT,  () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-//routes
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, '/cash-track/public/index.html'));
-});
-
-app.get('/log-in', function(req,res){
-    res.send(" This is the Log-In Page")
-});
-
-app.get('/sign-up', function(req,res){
-        res.send(" This is the Sign-up Page")
-});
