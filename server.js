ca021093c30b8f43fc79fd83689e8f4e0f6ff3e7
Steffano Malaga <smalagadev@gmail.com>
2019-07-27 10:41:27 -0400

    Routes updated

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 // Initialize Server
 app.listen(PORT,  () => console.log(`Server is running on port ${PORT}`));
 
+// Middleware
+app.use(express.static('public'));
+
 //routes
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '/cash-track/public/index.html'));
