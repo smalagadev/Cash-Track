e6c8bf4f2456c85dcdfe2974855c4d31d0122df0
Tbattle1911 <mr.battle1911@gmail.com>
2019-07-30 19:37:35 -0400

    git push

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,8 @@ const path = require("path");
 //exports
 module.exports = function(app){
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/cash-track/public/index.html"));
+    res.send("This is the Index File");
+    //res.sendFile(path.join(__dirname, "/cash-track/public/index.html"));
   });
 
   app.get("/log-in", function(req, res) {
