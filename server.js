7ad347874f3eae888316178b6ff77c75280f1fc6
Steffano Malaga <smalagadev@gmail.com>
2019-08-01 20:19:41 -0400

    Mongojs added

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 // Declare dependencies and variables
+const path = require('path');
+const mongo = require('mongojs');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Mongo DB section
+const db = mongo('cash-track-db', ['Users', 'Trips', 'Expenses']);
+
+db.on('error', function(error){
+  console.log(`Database Error: ${error}`);
+});
+
+app.get('/users', function(req, res){
+  db.Users.find({}, function(error, results){
+    error ? console.log(error) : res.json(results);
+  });
+});
+
 // Initialize Server
-app.listen(PORT,  () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT,  () => console.log(`Server is running on Port:${PORT}`));
 
 //routes
-app.get('/', function(req,res){
-    res.send("This is the Home Page")
-});
-
-app.get('/log-in', function(req,res){
-    res.send(" This is the Log-In Page")
-});
-
-app.get('/sign-up', function(req,res){
-        res.send(" This is the Sign-up Page")
-});
