eaab427fb63078760e39f8b8122e0dae3c4919dd
Jaden C Van <jcvan80@gmail.com>
2019-08-04 04:37:48 -0400

    finished nav

--- cash-track/src/App.js
+++ cash-track/src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Navbar from './components/Nav/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import Backdrop from './components/Backdrop/Backdrop';
 
-function App(){
+class App extends Component {
 
+    state = {
+        sidebarOpen: false
+    }
+
+    sidebarButtonClickHandler = () => {
+        this.setState((prevState) => {
+            return {sidebarOpen: !prevState.sidebarOpen}
+        })
+    }
+
+    backdropClickHandler = () => {
+        this.setState({sidebarOpen: false})
+    }
+
+    render() {
+        let backdrop;
+        let sidebar
+
+        if (this.state.sidebarOpen) {
+            backdrop = <Backdrop click={this.backdropClickHandler} />
+        }
+        return (
+            <div style = {{height: '100%'}}>
+                <Navbar sidebarButtonClickHandler={this.sidebarButtonClickHandler} />
+                <Sidebar show={this.state.sidebarOpen} />
+                {sidebar}
+                {backdrop}
+                <main style = {{marginTop: '64px'}}>
+                </main>
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ cash-track/src/components/Nav/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+import  SidebarButton from '../Sidebar/SidebarButton';
+
+const navbar = props => (
+    <header className = "navbar">
+        <nav className = "navbar_navigation">
+            <div className  = "sidebar_toggle_button">
+                <SidebarButton click={props.sidebarButtonClickHandler} />
+            </div>
+            <div className = "navbar_logo"><a href = "/">CashTrack</a></div>
+            {/* Space to make "CashTrack on left and items on right" */}
+            <div className = "emptySpace"/>
+            <div className = "navbar_navigation-items">
+                <ul>
+                    <li><a href = "/">Settings</a></li>
+                    <li><a href = "/">Manage Money</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default navbar
