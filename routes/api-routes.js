3e86b10fce3857d8931c683ab4065cf887820ad4
Steffano Malaga <smalagadev@gmail.com>
2019-08-04 23:07:48 -0400

    Exports fixed

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,26 @@
 const path = require('path');
+const db = require('./../models'); // Require all models
 
-exports.modules = function(app){
-  app.get('/api/', function(req, res){
+module.exports = function(app){
+  app.get('/users/', function(req, res){
     res.json({ thing: 'apple' });
-  })
+  });
+
+  app.post('/users/', function(req, res){
+    db.User.create(req.body)
+    .then(function(user){
+      res.json(user);
+    })
+    .catch(function(err){
+      res.json(err);
+    });
+  });
+
+  app.post('/trips/', function(req, res){
+
+  });
+
+  app.post('/expenses/', function(req, res){
+
+  });
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function(app){
   app.get('/', function(req, res){
-    res.sendFile('./../public/index.html');
+    res.sendFile(path.join(__dirname, '/../cash-track/public/index.html'));
   });
 
   app.get('/log-in', function(req,res){
