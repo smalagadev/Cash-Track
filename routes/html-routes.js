33b8df05058b84aa0bf1dfe16efff53c1b384a43
Steffano Malaga <smalagadev@gmail.com>
2019-08-05 21:20:08 -0400

    File path changed

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = function(app){
   app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '/../cash-track/public/index.html'));
+    // res.send(path.join(__dirname, '../cash-track/public/'));
+    res.sendFile(path.join(__dirname, '../cash-track/public/index.html'));
   });
 
   app.get('/log-in', function(req,res){
