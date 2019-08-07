189dc2ef8e80e4cd7eed20d1544948a40c3ba497
Steffano Malaga <smalagadev@gmail.com>
2019-08-07 15:47:43 -0400

    Client fixed

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App.js'
 
-ReactDom.render(<App />, document.getElementByID('root'));
+ReactDom.render(<App />, document.getElementById('root'));
