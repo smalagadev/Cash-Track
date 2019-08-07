40c04c9d936eae4b8df182627414118a26c44af7
Steffano Malaga <smalagadev@gmail.com>
2019-08-07 15:14:47 -0400

    Merge & Concurrently issue resolved

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = function(app){
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../cash-track/public/index.html"));
+
   app.get('/', function(req, res){
     // res.send(path.join(__dirname, '../cash-track/public/'));
     res.sendFile(path.join(__dirname, '../client/public/index.html'));
+
   });
 
   app.get('/log-in', function(req,res){
@@ -12,5 +17,6 @@ module.exports = function(app){
 
   app.get('/sign-up', function(req,res){
           res.send(" This is the Sign-up Page");
+  });
   });
 }
