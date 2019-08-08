a9609ec0b7c40bbb8b4ca59e5707dd1414f8a5eb
Steffano Malaga <smalagadev@gmail.com>
2019-08-08 18:11:42 -0400

    Demo

--- client/src/components/Logs/Logs.jsx
+++ client/src/components/Logs/Logs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Table } from 'reactstrap';
 import './Logs.css';
-import  demo from '../../demo';
+import demo from '../../demo.js';
 
 export default class Example extends React.Component {
   render() {
@@ -9,26 +9,61 @@ export default class Example extends React.Component {
       <Table hover className = "show-logs">
         <thead>
           <tr>
-            <th>#</th>
+            <th>Date</th>
             <th>Amount Spent</th>
-            <th>Category</th>
+            <th>Description</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">1</th>
-            <td>{demo.user.first}</td>
-            <td>Otto</td>
+            <th scope="row">{demo.expense[0].date}</th>
+            <td>{demo.expense[0].amount}</td>
+            <td>{demo.expense[0].description}</td>
           </tr>
           <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
+            <th scope="row">{demo.expense[1].date}</th>
+            <td>{demo.expense[1].amount}</td>
+            <td>{demo.expense[1].description}</td>
           </tr>
           <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
+            <th scope="row">{demo.expense[2].date}</th>
+            <td>{demo.expense[2].amount}</td>
+            <td>{demo.expense[2].description}</td>
+          </tr>
+          <tr>
+            <th scope="row">{demo.expense[3].date}</th>
+            <td>{demo.expense[3].amount}</td>
+            <td>{demo.expense[3].description}</td>
+          </tr>
+          <tr>
+            <th scope="row">{demo.expense[4].date}</th>
+            <td>{demo.expense[4].amount}</td>
+            <td>{demo.expense[4].description}</td>
+          </tr>
+          <tr>
+            <th scope="row">{demo.expense[5].date}</th>
+            <td>{demo.expense[5].amount}</td>
+            <td>{demo.expense[5].description}</td>
+          </tr>
+          <tr>
+            <th scope="row">{demo.expense[6].date}</th>
+            <td>{demo.expense[6].amount}</td>
+            <td>{demo.expense[6].description}</td>
+          </tr>
+          <tr>
+            <th scope="row">{demo.expense[7].date}</th>
+            <td>{demo.expense[7].amount}</td>
+            <td>{demo.expense[7].description}</td>
+          </tr>
+          <tr>
+            <th scope="row">{demo.expense[8].date}</th>
+            <td>{demo.expense[8].amount}</td>
+            <td>{demo.expense[8].description}</td>
+          </tr>
+          <tr>
+            <th scope="row">{demo.expense[9].date}</th>
+            <td>{demo.expense[9].amount}</td>
+            <td>{demo.expense[9].description}</td>
           </tr>
         </tbody>
       </Table>
